5cbd9d86acfa6965a617a48ba3fcb8b1d15de6c1
HARIPRAKASH1805 <58242393+HARIPRAKASH1805@users.noreply.github.com>
2022-01-01 20:06:23 +0530

    Add files via upload

---
+++ Basic programs/variables_iteration/datatypes.ino
@@ -0,0 +1,18 @@
+char num=90; 
+int num1=1;
+float num2=2.3;
+byte num3=222;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(num);
+  Serial.println(num1);
+  Serial.println(num2);
+  Serial.println(num3);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
